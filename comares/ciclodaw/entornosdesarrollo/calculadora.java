18df1e8acee48196706975ba64222c35c1d3437d
unknown <lspanc3009@iesmarquesdecomares.org>
2025-11-05 19:40:59 +0100

    subida definitiva

--- comares/ciclodaw/entornosdesarrollo/calculadora.java
+++ comares/ciclodaw/entornosdesarrollo/calculadora.java
@@ -3,8 +3,8 @@ package comares.ciclodaw.entornosdesarrollo;
 public class calculadora {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+System.out.println("suma ");
+System.out.println("resta");
 	}
 
 }
